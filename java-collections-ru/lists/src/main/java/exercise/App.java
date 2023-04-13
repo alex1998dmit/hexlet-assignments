74e85b987c73a1c9f180bdcafbaea64922833ed4
@hexlet/cli <support@hexlet.io>
2023-04-13 16:40:14 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -6,6 +6,26 @@ import java.util.List;
 
 // BEGIN
 class App {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+
+    public static boolean scrabble(String words, String word) {
+        List<String> wordsList = new ArrayList<String>(Arrays.asList(words.split("")));
+        List<String> wordList = new ArrayList<String>(Arrays.asList(word.split("")));
+
+        for (int i = 0; i < wordsList.size(); i++) {
+            wordsList.set(i, wordsList.get(i).toLowerCase());
+        }
+        for (String wordLetter: wordList) {
+            if (!wordsList.contains(wordLetter.toLowerCase())) {
+                return false;
+            } else {
+                wordsList.remove(wordLetter.toLowerCase());
+            }
+        }
+        return true;
+    }
 
 }
 //END
